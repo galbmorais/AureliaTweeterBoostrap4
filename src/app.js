7f62408b8d872c3706e3063a55ac4495f122974d
Gabriel <gabrielluiz.morais@hotmail.com>
2018-08-16 16:07:02 -0300

    Delete Tweet

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
 
+
+const ID = 0;
 const AVATAR = 'https://tinyurl.com/yb9d5vzk';
 const NAME = 'TechWow';
 const HANDLE = '@TechWow';
@@ -18,18 +20,21 @@ export class App {
   constructor() {
     this.tweets = [
       {
+        id: 0,
         avatar: 'https://tinyurl.com/yb9d5vzk',
         name: 'TechWow',
         handle: '@TechWow',
         text: 'Naval Ravikant on China money into Silicon Valley: This trickle could become a tsunami'
       },
       {
+        id: 1,
         avatar: 'https://pbs.twimg.com/profile_images/1332650890/strayfromthepath_flagtee_artworkslide_bigger.jpg',
         name: 'Stray From The Path',
         handle: '@strayfromdapath',
         text: 'This week has been such shit. The only thing that\'s made me happy is the impending STYG/Expire/KL tour and Shinsuke Nakamura entrance at NXT'
       },
       {
+        id: 2,
         avatar: 'https://cde.peru.com//ima/0/1/6/5/9/1659406/611x458/foto-perfil.jpg',
         name: 'Chris Sacca',
         handle: '@sacca',
@@ -41,6 +46,7 @@ export class App {
   createTweet() {
 
     this.tweets.unshift({
+      id: this.tweets.length + 1,
       avatar: AVATAR,
       name: NAME,
       handle: HANDLE,
@@ -51,8 +57,17 @@ export class App {
   }
 
   deleteTweet(tweet){
-    debugger;
+    let tweets = this.tweets; //array
+    let id = tweet.tweet.id; //id de quem quero remover
+    let tweetToRemove = tweets.filter(function(item) {
+      return item.id == id;
+    }); // retorna posicao no array
+    for(let tweet of tweetToRemove){
+      let index = tweets.indexOf(tweet);    
+      tweets.splice(index, 1); //remove tweet
+    }   
   }
+
 
   handleKeyPress(evt) {
     if (evt.keyCode === 13 && ths.composedTweet) {
--- src/resources/elements/tweet.js
+++ src/resources/elements/tweet.js
@@ -3,8 +3,5 @@ import {bindable} from 'aurelia-framework';
 export class Tweet {
   @bindable tweet;
 
-  deleteTweet(tweet){
-    debugger;
-  }
 }
 
