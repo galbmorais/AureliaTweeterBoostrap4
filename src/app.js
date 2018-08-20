ec8dae8a37098f5863f429b678b1dc4d41c71fb4
Gabriel <gabrielluiz.morais@hotmail.com>
2018-08-20 18:48:43 -0300

    Change User

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
 
-const ID = 0;
-const AVATAR = 'https://tinyurl.com/yb9d5vzk';
-const NAME = 'TechWow';
-const HANDLE = '@TechWow';
-
 export class App {
 
   attached() {
     $('[data-toggle="tooltip"]').tooltip();
   }
+  users = [
+    {avatar:'https://tinyurl.com/yb9d5vzk', name:'Tech Wow', handle:'@TechWow'},
+    {avatar:'https://pbs.twimg.com/profile_images/1332650890/strayfromthepath_flagtee_artworkslide_bigger.jpg', name:'From The Path', handle:'@fromdapath'},
+    {avatar:'https://cde.peru.com//ima/0/1/6/5/9/1659406/611x458/foto-perfil.jpg', name:'Chris Sacca', handle:'@sacca'}
+  ];
+  aux = 0;
 
   tweetToEdit = [{situation:null, id:null}];
   composedTweet = null;
@@ -23,21 +24,22 @@ export class App {
 
   constructor() {
 		this.myHeading = 'Aurelia Tweeter';
-		this.tweetBtn = 'Say it !';
+    this.tweetBtn = 'Say it !';
+    this.user = this.users[this.aux].name;
 
     this.tweets = [
       {
         id: 0,
         avatar: 'https://tinyurl.com/yb9d5vzk',
-        name: 'TechWow',
+        name: 'Tech Wow',
         handle: '@TechWow',
         text: 'Naval Ravikant on China money into Silicon Valley: This trickle could become a tsunami'
       },
       {
         id: 1,
         avatar: 'https://pbs.twimg.com/profile_images/1332650890/strayfromthepath_flagtee_artworkslide_bigger.jpg',
-        name: 'Stray From The Path',
-        handle: '@strayfromdapath',
+        name: 'From The Path',
+        handle: '@fromdapath',
         text: 'This week has been such shit. The only thing that\'s made me happy is the impending STYG/Expire/KL tour and Shinsuke Nakamura entrance at NXT'
       },
       {
@@ -49,6 +51,14 @@ export class App {
       }
     ];
   }
+
+   changeUser() {
+      let user = (this.aux + 1);
+      if( user >= this.users.length ) this.aux = 0;
+      else this.aux = user;
+      this.user = this.users[this.aux].name;
+   }
+
   default() {
     this.composedTweet = null;
     this.tweetToEdit.situation = false;
@@ -58,22 +68,23 @@ export class App {
 
   createTweet() {
     if (this.tweetToEdit.situation == true){
-      let tweets = this.tweets; //array  
+      let tweets = this.tweets; //array
       let id = this.tweetToEdit.id;
       for(this.tweetSelecionado of tweets){
         if(this.tweetSelecionado.id == id){
-          let index = this.tweets.indexOf(this.tweetSelecionado);    
+          let index = this.tweets.indexOf(this.tweetSelecionado);
           tweets[index].text = this.composedTweet;
           this.edited = true;
-          setTimeout(() => this.edited = false, 2000); 
+          setTimeout(() => this.edited = false, 2000);
         }
       }
     } else {
+      let user = this.users[this.aux];
       this.tweets.unshift({
         id: this.lastId + 1,
-        avatar: AVATAR,
-        name: NAME,
-        handle: HANDLE,
+        avatar: user.avatar,
+        name: user.name,
+        handle: user.handle,
         text: this.composedTweet
       });
       this.lastId++;
@@ -89,11 +100,11 @@ export class App {
       return item.id == id;
     }); // retorna obj
     for(let tweet of tweetToRemove){
-      let index = tweets.indexOf(tweet);    
+      let index = tweets.indexOf(tweet);
       tweets.splice(index, 1); //remove tweet
       this.removed = true;
       setTimeout(() => this.removed = false, 1000);
-    }   
+    }
   }
 
   editTweet(tweet){
