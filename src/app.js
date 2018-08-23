8b34b93ce94e55e3bda521c542a27ad564884f90
Gabriel <gabrielluiz.morais@hotmail.com>
2018-08-23 19:47:35 -0300

    Details

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ export class App {
     configureRouter(config, router) {
         config.title = 'Aurelia';
         config.map([
-            { route: ['', 'tweets'], name: 'tweets', moduleId: 'tweeter', nav: true,  title:'Tweeter' },
-            { route: 'home',         name: 'teste',   moduleId: 'teste',  nav: true,  title:'Teste'   }
+            { route: ['', ''], redirect: 'tweeter' },
+            { route: 'tweeter', name: 'tweets', moduleId: 'tweeter', nav: true,  title:'Tweeter' },
+            { route: 'teste',    name: 'teste',  moduleId: 'teste',  nav: true,  title:'Teste'   }
         ]);
         this.router = router;
     }
